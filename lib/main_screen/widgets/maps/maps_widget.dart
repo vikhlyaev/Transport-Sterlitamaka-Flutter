816ad86dd233d2d9a75c8c996df507f949810b18
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-01-22 19:41:30 +0500

    Adds maps screen

---
+++ lib/main_screen/widgets/maps/maps_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:transport_sterlitamaka/theme/user_colors.dart';
+
+class MapsWidget extends StatelessWidget {
+  const MapsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
