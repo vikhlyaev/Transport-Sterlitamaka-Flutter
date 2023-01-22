9ba0c548c834ec535ae6f8f6030d7a3dacac48af
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-01-22 19:41:00 +0500

    Adds favorites screen

---
+++ lib/main_screen/widgets/favorites/favorites_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:transport_sterlitamaka/theme/user_colors.dart';
+
+class FavoritesWidget extends StatelessWidget {
+  const FavoritesWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: UserColors.red,
+        title: Text('Избранное'),
+      ),
+      body: Container(),
+    );
+  }
+}
