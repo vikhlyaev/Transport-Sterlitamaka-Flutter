f066d9c604d3a457a3292d2140b48fe833d9fb7b
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-01-22 19:41:17 +0500

    Adds routes screen

---
+++ lib/main_screen/widgets/routes/routes_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RoutesWidget extends StatelessWidget {
+  const RoutesWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Маршруты'),
+      ),
+      body: Container(),
+    );
+  }
+}
