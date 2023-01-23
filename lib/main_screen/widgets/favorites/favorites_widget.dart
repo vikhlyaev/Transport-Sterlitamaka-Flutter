0070a7e33c388cc7713e402fd1d206547bf5d4df
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-01-23 12:30:22 +0500

    Adds favorites screen

--- lib/main_screen/widgets/favorites/favorites_widget.dart
+++ lib/main_screen/widgets/favorites/favorites_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:transport_sterlitamaka/main_screen/widgets/routes/widgets/route_button_widget.dart';
+import 'package:transport_sterlitamaka/main_screen/widgets/stations/widgets/station_cell_widget.dart';
 import 'package:transport_sterlitamaka/theme/user_colors.dart';
 
 class FavoritesWidget extends StatelessWidget {
@@ -9,9 +11,53 @@ class FavoritesWidget extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: UserColors.blue,
-        title: Text('Избранное'),
+        title: const Text('Избранное'),
       ),
-      body: Container(),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        padding: const EdgeInsets.all(16.0),
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        children: [
+          Text(
+            'ОСТАНОВКИ',
+            style: Theme.of(context).textTheme.titleSmall,
+          ),
+          const SizedBox(height: 16),
+          ListView.separated(
+            scrollDirection: Axis.vertical,
+            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              return const StationCellWidget();
+            },
+            separatorBuilder: (context, index) => const Divider(
+              color: Color(0xFFD9D9D9),
+              height: 1,
+            ),
+          ),
+          const SizedBox(height: 32),
+          Text(
+            'МАРШРУТЫ',
+            style: Theme.of(context).textTheme.titleSmall,
+          ),
+          const SizedBox(height: 16),
+          GridView.count(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            mainAxisSpacing: 16,
+            crossAxisSpacing: 16,
+            crossAxisCount: 5,
+            children: const [
+              RouteButtonWidget(),
+              RouteButtonWidget(),
+              RouteButtonWidget(),
+              RouteButtonWidget(),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
