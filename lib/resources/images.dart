79657da971b6e618efec3b04e3ff97e46b93d71e
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-01-24 14:14:31 +0500

    Fixes image

--- lib/resources/images.dart
+++ lib/resources/images.dart
@@ -3,6 +3,7 @@ part of 'resources.dart';
 class Images {
   Images._();
 
+  static const String card = 'assets/images/card.png';
   static const String iconExpense = 'assets/images/icon_expense.png';
   static const String iconIncome = 'assets/images/icon_income.png';
   static const String iconStation = 'assets/images/icon_station.png';
