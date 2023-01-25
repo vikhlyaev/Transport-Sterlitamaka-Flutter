ec7273eca692557b8df22ccd0d12243d90504799
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-01-25 21:42:47 +0500

    Removes search button, add mapbox map

--- lib/screens/maps/maps_widget.dart
+++ lib/screens/maps/maps_widget.dart
@@ -1,16 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'package:transport_sterlitamaka/secrets.dart';
 import 'package:transport_sterlitamaka/theme/user_colors.dart';
+import 'package:turf/helpers.dart';
 
-class MapsWidget extends StatelessWidget {
+class MapsWidget extends StatefulWidget {
   const MapsWidget({super.key});
+
+  @override
+  State<MapsWidget> createState() => _MapsWidgetState();
+}
+
+class _MapsWidgetState extends State<MapsWidget> {
+  MapboxMap? mapboxMap;
+
+  _onMapCreated(MapboxMap mapboxMap) {
+    this.mapboxMap = mapboxMap;
+    _setLocale();
+    _setupMap();
+    _getUserLocation();
+  }
+
+  void _setLocale() {
+    mapboxMap?.style.localizeLabels('ru', null);
+  }
+
+  void _getUserLocation() {
+    mapboxMap?.location
+        .updateSettings(LocationComponentSettings(enabled: true));
+  }
+
+  void _setupMap() {
+    mapboxMap?.compass.updateSettings(CompassSettings(enabled: false));
+    mapboxMap?.scaleBar.updateSettings(ScaleBarSettings(enabled: false));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          Container(
-            color: UserColors.green,
+          // 53.632400, 55.953775
+          MapWidget(
+            resourceOptions: ResourceOptions(accessToken: Secrets.ACCESS_TOKEN),
+            cameraOptions: CameraOptions(
+              center:
+                  Point(coordinates: Position(55.953775, 53.632400)).toJson(),
+              zoom: 12.0,
+            ),
+            styleUri: MapboxStyles.LIGHT,
+            onMapCreated: _onMapCreated,
           ),
           Positioned(
             top: 60,
@@ -39,31 +78,6 @@ class MapsWidget extends StatelessWidget {
                   Text('Добавить карту'),
                 ],
               ),
-            ),
-          ),
-          Positioned(
-            top: 60,
-            left: 16,
-            child: ElevatedButton(
-              onPressed: () => print('tap'),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.white),
-                foregroundColor: MaterialStateProperty.all(UserColors.blue),
-                overlayColor:
-                    MaterialStateProperty.all(UserColors.blue.withAlpha(20)),
-                minimumSize: MaterialStateProperty.all(
-                  const Size(44, 44),
-                ),
-                padding: MaterialStateProperty.all(
-                  const EdgeInsets.symmetric(vertical: 10),
-                ),
-                shape: MaterialStateProperty.all(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(100),
-                  ),
-                ),
-              ),
-              child: const Icon(Icons.search),
             ),
           ),
         ],
