e4d5d09fb0d9c9c05f415de675a4e7e6ee7a8352
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-01-28 18:39:52 +0500

    Adds icons

--- lib/resources/images.dart
+++ lib/resources/images.dart
@@ -4,7 +4,12 @@ class Images {
   Images._();
 
   static const String card = 'assets/images/card.png';
+  static const String iconBus = 'assets/images/icon_bus.png';
   static const String iconExpense = 'assets/images/icon_expense.png';
   static const String iconIncome = 'assets/images/icon_income.png';
   static const String iconStation = 'assets/images/icon_station.png';
+  static const String iconStationActive =
+      'assets/images/icon_station_active.png';
+  static const String iconStationList = 'assets/images/icon_station_list.png';
+  static const String iconTrolleybus = 'assets/images/icon_trolleybus.png';
 }
