a72ec6a24b6047750e84f96f4e1a52c6dd909b48
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-01-28 15:27:00 +0500

    Adds Scheme model

---
+++ lib/models/scheme.dart
@@ -0,0 +1,11 @@
+class Scheme {
+  Scheme({
+    required this.routeName,
+    required this.pointLatitude,
+    required this.pointLongitude,
+  });
+
+  int routeName;
+  double pointLatitude;
+  double pointLongitude;
+}
