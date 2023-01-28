bc475fc1aa174d5242ca8ba84f0b71500c56bbb1
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-01-28 15:30:45 +0500

    Adds Route model

---
+++ lib/models/route.dart
@@ -0,0 +1,13 @@
+class Route {
+  Route({
+    required this.id,
+    required this.name,
+    required this.descId,
+    required this.isFavorite,
+  });
+
+  int id;
+  String name;
+  String descId;
+  bool isFavorite;
+}
