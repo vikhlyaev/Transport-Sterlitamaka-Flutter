c53729629ce3796b422c9bbcaa471f696af412c6
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-01-30 16:26:14 +0500

    Adds an extension for the MapboxMapController class

---
+++ lib/extensions/for_custom_symbols_extension.dart
@@ -0,0 +1,33 @@
+import 'package:mapbox_gl/mapbox_gl.dart';
+import 'package:transport_sterlitamaka/models/station_symbol.dart';
+import 'package:transport_sterlitamaka/models/station_symbol_options.dart';
+import 'package:transport_sterlitamaka/models/track_symbol.dart';
+import 'package:transport_sterlitamaka/models/track_symbol_options.dart';
+
+/// Расширение класса `MapboxMapController` - добавили отдельные методы для кастомных маркеров.
+/// Маркер забирает в себя необходимый свойства из `SymbolOptions`, например id остановки и ее название
+extension forCustomSymbols on MapboxMapController {
+  Future<List<StationSymbol>> addStationSymbols(List<StationSymbolOptions> options, [List<Map>? data]) async {
+    final symbols = [
+      for (var i = 0; i < options.length; i++)
+        StationSymbol(getRandomString(), SymbolOptions.defaultOptions.copyWith(options[i]), options[i].id,
+            options[i].name, data?[i])
+    ];
+    await symbolManager!.addAll(symbols);
+
+    notifyListeners();
+    return symbols;
+  }
+
+  Future<List<TrackSymbol>> addTrackSymbols(List<TrackSymbolOptions> options, [List<Map>? data]) async {
+    final symbols = [
+      for (var i = 0; i < options.length; i++)
+        TrackSymbol(getRandomString(), SymbolOptions.defaultOptions.copyWith(options[i]), options[i].id,
+            options[i].route, data?[i])
+    ];
+    await symbolManager!.addAll(symbols);
+
+    notifyListeners();
+    return symbols;
+  }
+}
