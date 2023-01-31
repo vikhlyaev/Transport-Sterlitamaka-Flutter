7d1446d710b1f79a80ea3349df1617be50c99de0
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-01-31 16:20:39 +0500

    Adds support for favorites

--- lib/screens/routes/routes_widget.dart
+++ lib/screens/routes/routes_widget.dart
@@ -1,67 +1,65 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:transport_sterlitamaka/screens/routes/widgets/route_button_widget.dart';
+import 'package:transport_sterlitamaka/utils/apihelper.dart';
+import 'package:transport_sterlitamaka/utils/dbhelper.dart';
 
 class RoutesWidget extends StatelessWidget {
   const RoutesWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
+    SliverGridDelegate delegate =
+        const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5, crossAxisSpacing: 16, mainAxisSpacing: 16);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Маршруты'),
       ),
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        padding: const EdgeInsets.all(16.0),
-        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-        children: [
-          Text(
-            'ТРОЛЛЕЙБУСЫ',
-            style: Theme.of(context).textTheme.titleSmall,
-          ),
-          const SizedBox(height: 16),
-          GridView.count(
-            physics: const NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            mainAxisSpacing: 16,
-            crossAxisSpacing: 16,
-            crossAxisCount: 5,
-            children: const [
-              // RouteButtonWidget(),
-              // RouteButtonWidget(),
-              // RouteButtonWidget(),
-              // RouteButtonWidget(),
-              // RouteButtonWidget(),
-              // RouteButtonWidget(),
-              // RouteButtonWidget(),
-              // RouteButtonWidget(),
-              // RouteButtonWidget(),
-              // RouteButtonWidget(),
-              // RouteButtonWidget(),
-              // RouteButtonWidget(),
-              // RouteButtonWidget(),
-            ],
-          ),
-          const SizedBox(height: 32),
-          Text(
-            'АВТОБУСЫ',
-            style: Theme.of(context).textTheme.titleSmall,
-          ),
-          const SizedBox(height: 16),
-          GridView.count(
-            physics: const NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            mainAxisSpacing: 16,
-            crossAxisSpacing: 16,
-            crossAxisCount: 5,
-            children: const [
-              // RouteButtonWidget(),
-              // RouteButtonWidget(),
-              // RouteButtonWidget(),
-              // RouteButtonWidget(),
-            ],
-          ),
-        ],
+      body: FutureBuilder(
+        future: DBHelper.instance.routes,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final trolleybuses = snapshot.data!.where((e) => e.name < 30).toList();
+            final buses = snapshot.data!.where((e) => e.name > 30).toList();
+            return ListView(
+              scrollDirection: Axis.vertical,
+              padding: const EdgeInsets.all(16.0),
+              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+              children: [
+                Text(
+                  'ТРОЛЛЕЙБУСЫ',
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+                const SizedBox(height: 16),
+                GridView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  gridDelegate: delegate,
+                  itemCount: trolleybuses.length,
+                  itemBuilder: (context, index) => RouteButtonWidget(route: trolleybuses[index]),
+                ),
+                const SizedBox(height: 32),
+                Text(
+                  'АВТОБУСЫ',
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+                const SizedBox(height: 16),
+                GridView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  gridDelegate: delegate,
+                  itemCount: buses.length,
+                  itemBuilder: (context, index) => RouteButtonWidget(route: buses[index]),
+                ),
+              ],
+            );
+          } else {
+            return const Center(
+              child: CupertinoActivityIndicator(),
+            );
+          }
+        },
       ),
     );
   }
